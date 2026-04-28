6f09dcc0fbf39599dd9f44d228ea77e937b9422c
Demarthias <grayson@venom-industries.com>
2026-04-28 08:19:35 -0600

    updates

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@ import { trackEvent } from './analytics';
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     trackEvent('extension_installed');
+    chrome.tabs.create({ url: chrome.runtime.getURL('onboarding.html') });
   } else if (details.reason === 'update') {
     trackEvent('extension_updated', { version: chrome.runtime.getManifest().version });
   }
